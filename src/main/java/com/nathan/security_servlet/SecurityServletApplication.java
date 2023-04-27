44946af69f4c0d9134f50a68e229eea66fbf1669
followdeklerk <nathandeklerk@gmx.com>
2023-04-27 13:10:34 +0200

    Init Commit

---
+++ src/main/java/com/nathan/security_servlet/SecurityServletApplication.java
@@ -0,0 +1,13 @@
+package com.nathan.security_servlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityServletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityServletApplication.class, args);
+    }
+
+}
